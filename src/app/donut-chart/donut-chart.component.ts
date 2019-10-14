5ca2f7516aabf3541a6ba71211d8d71078c827bb
IsaakNazar <nazarchampion@gmail.com>
2019-10-14 08:21:44 +0600

    donut

--- src/app/donut-chart/donut-chart.component.ts
+++ src/app/donut-chart/donut-chart.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartType } from 'chart.js';
-import { Label, MultiDataSet } from 'ng2-charts';
+import {Component, OnInit} from '@angular/core';
+import {ChartType} from 'chart.js';
+import * as d3 from 'd3';
 
 // import Chart from 'chart.js';
 
@@ -10,26 +10,55 @@ import { Label, MultiDataSet } from 'ng2-charts';
   styleUrls: ['./donut-chart.component.scss']
 })
 export class DonutChartComponent implements OnInit {
-  public doughnutChartData = [];
-  public bg = ['#3e95cd', '#8e5ea2', '#3cba9f', '#e8c3b9'];
+  data = [
+    {platform: 'Android', percentage: 40.11},
+    {platform: 'Windows', percentage: 36.69},
+    {platform: 'iOS', percentage: 13.06}
+  ];
+
+  svgWidth = 500;
+  svgHeight = 300;
+  radius = Math.min(this.svgWidth, this.svgHeight) / 2;
 
   constructor() {
   }
 
   ngOnInit() {
 
-    this.doughnutChartData = [{
-      data: [350, 450, 100, 125],
-      backgroundColor: this.bg
-    }];
-  }
+    const svg = d3.select('svg')
+      .attr('width', this.svgWidth)
+      .attr('height', this.svgHeight);
 
-  // public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
-  //   console.log(event, active);
-  // }
-  //
-  // public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
-  //   console.log(event, active);
-  // }
+// Create group element to hold pie chart
+    const g = svg.append('g')
+      .attr('transform', 'translate(' + this.radius + ',' + this.radius + ')');
+
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    const pie = d3.pie().value(d => d.percentage);
+
+    const path = d3.arc()
+      .outerRadius(this.radius)
+      .innerRadius(this.radius - 40);
+
+    const arc = g.selectAll('arc')
+      .data(pie(this.data))
+      .enter()
+      .append('g');
+
+    arc.append('path')
+      .attr('d', path)
+      .attr('fill', d => color(d.data.percentage));
+
+    //
+    // const label = d3.arc()
+    //   .outerRadius(this.radius)
+    //   .innerRadius(0);
+    //
+    // arc.append('text')
+    //   .attr('transform', () =>  'translate(' + label + ')')
+    //   .attr('text-anchor', 'middle')
+    //   .text(d => d.data.platform + ':' + d.data.percentage + '%');
+  }
 
 }
